ddc6fe1d1f83e5bec6dc073e36a3948973249af4
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-04-21 14:01:49 +0530

    Remove TypeScript types and simplify handleComplete

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,7 @@
-'use client'
 import React, { useEffect } from 'react'
 import Child from './comp/Child'
 import { todo } from 'node:test'
-type todoArr={
-    id:number,
-    task:string,
-    status:string
-}
+
 const todos=[
     { id:1,task:"task1",status:"pending"},
     { id:2,task:"task2",status:"pending"},
@@ -15,7 +10,7 @@ const todos=[
 
 const page = () => {
     const [todo,setTodo]=React.useState(todos)
-    const handleComplete=(id:number)=>{
+    const handleComplete=(id)=>{
         setTodo(prev=>prev.map((t)=>t.id===id?{...t,status:"completed"}:t))
         
     }

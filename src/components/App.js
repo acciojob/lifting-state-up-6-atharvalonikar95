05859a77ae6e0e688b9b4f6a075d676f73ddbd89
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-04-21 14:01:05 +0530

    Rename App component to page and update logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,30 @@
+'use client'
+import React, { useEffect } from 'react'
+import Child from './comp/Child'
+import { todo } from 'node:test'
+type todoArr={
+    id:number,
+    task:string,
+    status:string
+}
+const todos=[
+    { id:1,task:"task1",status:"pending"},
+    { id:2,task:"task2",status:"pending"},
+    { id:3,task:"task3",status:"pending"},
+]
 
-import React from "react";
-import './../styles/App.css';
+const page = () => {
+    const [todo,setTodo]=React.useState(todos)
+    const handleComplete=(id:number)=>{
+        setTodo(prev=>prev.map((t)=>t.id===id?{...t,status:"completed"}:t))
+        
+    }
 
-const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <Child todos={todo} handleComplete={handleComplete} />
     </div>
   )
 }
 
-export default App
+export default page
